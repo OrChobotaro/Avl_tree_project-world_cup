056c28fcb6fa9b40cf3807e99795938fdcd122d2
ShaharKadosh <shaharkadosh@campus.technion.ac.il>
2022-11-20 21:51:44 +0200

    Height and Insert after testing

--- main.cpp
+++ main.cpp
@@ -3,9 +3,12 @@
 int main(){
 
     AvlTree<int> tree1;
-
-    Node<int> *node = new Node<int>(3);
-    tree1.insert(node, 2);
+    tree1.insert(nullptr, 89);
+    tree1.insert(tree1.getRoot(), 22);
+    tree1.insert(tree1.getRoot(), 54);
+    tree1.insert(tree1.getRoot(), 103);
+    tree1.insert(tree1.getRoot(), 76);
+    tree1.insert(tree1.getRoot(), 100);
 
     return 0;
 }
